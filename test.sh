d4d441b06a7f2e88b3f27c144c7f5c4ff0b602f2
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-08-28 09:28:33 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+echo "Hello, Testing"
+
+
+# Step 2:  
+# Authenticate & tag
+echo "Learning"
+
+# Step 3:
+# Push image to a docker repository
+echo "docker
