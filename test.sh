e8281d560cbff9a30c63c1081fb8d3775c566a9f
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-09 13:30:57 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 echo "Hello, Testing"
+k="test"
 
 
 # Step 2:  
 # Authenticate & tag
 echo "Learning"
+echo $k
 
 # Step 3:
 # Push image to a docker repository
-echo "docker tester">/home/ubuntu/out.txt
+echo "docker tester"
